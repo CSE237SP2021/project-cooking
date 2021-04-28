dea9770d725aec1fcfac848430a788ddaddace6e
Dominic Bottom <dbottom@wustl.edu>
2021-04-27 21:27:33 -0500

    Add error handling for the export method in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,7 +13,21 @@ public class Main {
 		String response = controller.initialize();
 		if (response.equals("1")) {
 			String text = aPIController.getRandomPopularRecipe();
-			exporter.export(text);
+			switch (exporter.export(text)) 
+			{
+				case success:
+					System.out.println("...and we got it! You'll find it in the recipes folder :)");
+					break;
+				case parse_failure:
+				case missing_fields_failure:
+					System.out.println("...but there was an issue with it, so we weren't able to grab it for you. Sorry about that :(");
+					break;
+				case file_creation_failure:
+					System.out.println("...but we had trouble making a new file for it, so it was lost. Sorry about that :(");
+				default:
+					System.out.println("...but we ran into some trouble, so it was lost. Sorry about that :(");
+					break;
+			}
 			controller.continuePrompt();
 		}
 	}
